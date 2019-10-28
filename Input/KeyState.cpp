6c622131e059fe1a4bb2fc3c7d363f67329d8b5c
tekito <tekito@kk.kk>
2019-10-28 15:18:19 +0900

    10/28

--- Input/KeyState.cpp
+++ Input/KeyState.cpp
@@ -15,9 +15,26 @@ KeyState::KeyState()
 	_keyConDef.emplace_back(KEY_INPUT_A);
 	_keyConDef.emplace_back(KEY_INPUT_S);
 
+
+	FILE* fp = NULL;
+	if (fopen_s(&fp, "key.dat", "rb") != 0)
+	{
+		for (int i = 0; i < ; i++)
+		{
+
+		}
+	}
+	else
+	{
+		fread(, sizeof(), , fp);
+
+		fclose(fp);
+	}
+
+
 	_keyCon = _keyConDef;
 	modoKeyOld = 1;
-
+	
 	func = &KeyState::RefKeyData;
 }
 
@@ -56,10 +73,10 @@ void KeyState::SetKeyConfig(void)
 		func = &KeyState::RefKeyData;
 	}
 
-	auto CheckKey = []() {
-		for ()
+	auto checkKey = [&](int id) {
+		for (auto i = INPUT_ID::LEFT; i < _confID; i++)
 		{
-			if ()
+			if (_keyCon[static_cast<int>(i)] == id)
 			{
 				return false;
 			}
@@ -67,22 +84,30 @@ void KeyState::SetKeyConfig(void)
 		return true;
 	};
 
-	for (int i = 0; i < sizeof(_buf); i++)
+	for (int id = 0; id < sizeof(_buf); id++)
 	{
-		if (!CheckKey())
+		if (!checkKey(id))
 		{
 			continue;
 		}
 
-		if (_buf[i])
+		if (_buf[id] && !_buf[KEY_INPUT_F1])
 		{
-			_keyCon[static_cast<int>(_confID)] = i;
+			_keyCon[static_cast<int>(_confID)] = id;
 			++_confID;
+
+			if (_confID == end(_confID))
+			{
+				FILE* fp = NULL;
+				if (fopen_s(&fp, "key.dat", "wb") == 0)
+				{
+					fwrite(, sizeof(), , fp);
+
+					fclose(fp);
+				}
+				TRACE("RefKeyData\n")
+				func = &KeyState::RefKeyData;
+			}
 		}
-	}
-	if (_confID == INPUT_ID::MAX)
-	{
-		TRACE("RefKeyData\n")
-		func = &KeyState::RefKeyData;
 	}
 }
